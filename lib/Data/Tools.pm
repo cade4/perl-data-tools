e52884759a63ca7e861da72ec1366148acc205ca
Vladi Belperchinov-Shabanski <cade@datamax.bg>
2013-03-06 03:59:34 +0200

    initial-docs-and-few-fixes

--- lib/Data/Tools.pm
+++ lib/Data/Tools.pm
@@ -11,8 +11,9 @@
 package Data::Tools;
 
 use Exporter;
-@ISA    = qw( Exporter );
-@EXPORT = qw(
+our $VERSION = 1.0;
+our @ISA    = qw( Exporter );
+our @EXPORT = qw(
               file_save
               file_load
               
@@ -35,11 +36,18 @@ use Exporter;
               perl_package_to_file
 
             );
+
+our %EXPORT_TAGS = (
+                   
+                   'all' => \@EXPORT,
+                   
+                   );
+            
 use strict;
 
 ##############################################################################
 
-sub load_file
+sub file_load
 {
   my $fn = shift; # file name
   
@@ -51,7 +59,7 @@ sub load_file
   return $s;
 }
 
-sub save_file
+sub file_save
 {
   my $fn = shift; # file name
 
@@ -212,6 +220,50 @@ INIT  { __url_escapes_init(); }
 =pod
 
 
+=head1 NAME
+
+Exception::Sink - general purpose compact exception handling.
+
+=head1 SYNOPSIS
+
+  use Data::Tools qw( :all );
+
+  my $res  = file_save( $file_name, 'file data here' );
+  my $data = file_load( $file_name );
+  
+  my $res  = dir_path_make( '/path/to/somewhere' ); # create full path with 0700
+  my $path = dir_path_ensure( '/path/s/t/h' ); # ensure path exists, check+make
+  
+  my $escaped   = str_url_escape( $plain_str ); # url-style %XX escaping
+  my $plain_str = str_url_unescape( $escaped );
+  
+  my $hex_str   = str_hex( $plain_str ); # hex-style string escaping
+  my $plain_str = str_unhex( $hex_str );
+  
+  my $hash_str = hash2str( $hash_reference ); # convert hash to string "key=value\n"
+  my $hash_reference = str2hash( $hash_str );
+  
+  # save/load hash in str_url_escaped form to/from a file
+  my $res            = hash_save( $file_name, $hash_reference );
+  my $hash_reference = hash_load( $file_name );
+  
+  my $perl_pkg_fn = perl_package_to_file( 'Data::Tools' ); # returns "Data/Tools.pm"
+
+=head1 FUNCTIONS
+
+  (more docs)
+
+=head1 TODO
+
+  (more docs)
+
+=head1 AUTHOR
+
+  Vladi Belperchinov-Shabanski "Cade"
+
+  <cade@biscom.net> <cade@datamax.bg> <cade@cpan.org>
+
+  http://cade.datamax.bg
 
 =cut
 
